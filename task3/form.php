ff34feb0dcf4c8288449a583b64ef6e2b350e42a
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-02-27 16:43:12 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -6,7 +6,62 @@
       printf('<option value="%d">%d год</option>', $i, $i);
     }
     ?>
+<label>
+          Телефон:<br />
+          <input
+            name="telephone"
+            type="tel"
+            placeholder="Введите номер телефона"
+          /> </label
+        ><br />
+        <label>
+          Email:<br />
+          <input
+            name="email"
+            type="email"
+            placeholder="Введите вашу почту"
+          /> </label
+        ><br />
+        <br />
+        <label>
+          Дата рождения:<br />
+          <input name="field-date" value="2000-01-01" type="date" /> </label
+        ><br />
+        <br />
+        Ваш пол:<br />
+        <label
+          ><input
+            type="radio"
+            checked="checked"
+            name="radio-group-1"
+            value="м"
+          />
+          Мужчина</label
+        >
+        <br />
+        <label
+          ><input type="radio" name="radio-group-1" value="ж" /> Женщина</label
+        ><br />
+        <br />
+        <label>
+          Выберете любимый язык программирования:<br />
+          <select name="field-name-3" multiple="multiple">
+            <option value="value1" selected="selected">Pascal</option>
+            <option value="value2">C</option>
+            <option value="value3">C++</option>
+            <option value="value4">JavaScript</option>
+            <option value="value5">PHP</option>
+            <option value="value6">Python</option>
+            <option value="value4">Java</option>
+            <option value="value5">Haskel</option>
+            <option value="value6">Clojure</option>
+            <option value="value4">Prolog</option>
+            <option value="value5">Scala</option>
+          </select> </label
+        ><br />
+        <br />
   </select>
   
   <input type="submit" value="ok" />
 </form>
+
