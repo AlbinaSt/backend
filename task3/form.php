b4c9f2dfd33e83013599a4e547832f956c6a5635
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-03-05 16:21:24 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -43,23 +43,23 @@
           ><input type="radio" name="radio-1" value="ж" /> Женщина</label
         ><br />
         <br />
-        <label>
-          Выберете любимый язык программирования:<br />
-          <select name="field-name-3" multiple="multiple">
-            <option value="value1" selected="selected">Pascal</option>
-            <option value="value2">C</option>
-            <option value="value3">C++</option>
-            <option value="value4">JavaScript</option>
-            <option value="value5">PHP</option>
-            <option value="value6">Python</option>
-            <option value="value4">Java</option>
-            <option value="value5">Haskel</option>
-            <option value="value6">Clojure</option>
-            <option value="value4">Prolog</option>
-            <option value="value5">Scala</option>
-          </select> </label
-        ><br />
-        <br />
+    //    <label>
+     //     Выберете любимый язык программирования:<br />
+     //     <select name="field-name-3" multiple="multiple">
+      //      <option value="value1" selected="selected">Pascal</option>
+      //      <option value="value2">C</option>
+      //      <option value="value3">C++</option>
+       //     <option value="value4">JavaScript</option>
+       //     <option value="value5">PHP</option>
+       //     <option value="value6">Python</option>
+        //    <option value="value4">Java</option>
+        //    <option value="value5">Haskel</option>
+        //    <option value="value6">Clojure</option>
+      //      <option value="value4">Prolog</option>
+      //      <option value="value5">Scala</option>
+      //    </select> </label
+      //  ><br />
+       // <br />
    <label
           ><input type="checkbox" checked="checked" name="check-1" /> С
           контрактом ознакомлен</label
