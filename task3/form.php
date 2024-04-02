178ee47ce434ca1a81fa3a185c24fe6cb3abb238
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-04-02 16:33:53 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -9,21 +9,35 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.js"></script>
 <link rel="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/css/bootstrap-multiselect.css" type="text/css"/>
 <style>
-  body{background:#556B2F;margin:0}
+  .error {
+  border: 2px solid red;
+}
 
-.form{width:400px;height:auto;background:#e6e6e6;border-radius:8px;box-shadow:0 0 40px -10px #000; margin: calc(50vh - 220px) auto; padding:20px 30px;max-width:calc(100vw - 40px);box-sizing:border-box;font-family:'Montserrat',sans-serif;position:relative}
+  body{
+    background:#556B2F;
+    margin:0
+  }
 
-h2{margin:10px 0;padding-bottom:10px;width:180px;color:#78788c;border-bottom:3px solid #78788c}
+.form{
+  width:400px;height:auto;background:#e6e6e6;border-radius:8px;box-shadow:0 0 40px -10px #000; margin: calc(50vh - 220px) auto; padding:20px 30px;max-width:calc(100vw - 40px);box-sizing:border-box;font-family:'Montserrat',sans-serif;position:relative}
 
-input1{width:100%;padding:10px;box-sizing:border-box;background:none;outline:none;resize:none;border:0;font-family:'Montserrat',sans-serif;transition:all .3s;border-bottom:2px solid #bebed2}
+h2{
+  margin:10px 0;padding-bottom:10px;width:180px;color:#78788c;border-bottom:3px solid #78788c}
 
-input1:focus{border-bottom:2px solid #78788c}
+input1{
+  width:100%;padding:10px;box-sizing:border-box;background:none;outline:none;resize:none;border:0;font-family:'Montserrat',sans-serif;transition:all .3s;border-bottom:2px solid #bebed2}
 
-p:before{content:attr(type);display:block;margin:28px 0 0;font-size:14px;color:#5a5a5a}
+input1:focus
+  {border-bottom:2px solid #78788c}
 
-button{float:right;padding:8px 12px;margin:8px 0 0;font-family:'Montserrat',sans-serif;border:2px solid #78788c;background:0;color:#5a5a6e;cursor:pointer;transition:all .3s}
+p:before
+  {content:attr(type);display:block;margin:28px 0 0;font-size:14px;color:#5a5a5a}
 
-button:hover{background:#78788c;color:#fff}
+button
+  {float:right;padding:8px 12px;margin:8px 0 0;font-family:'Montserrat',sans-serif;border:2px solid #78788c;background:0;color:#5a5a6e;cursor:pointer;transition:all .3s}
+
+button:hover
+  {background:#78788c;color:#fff}
 
 .radio-1 { 
   display: inline;
