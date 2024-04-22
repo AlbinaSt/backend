b276fa946b5aa5c331c6488368b9c94696e8a6c5
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-04-22 19:38:11 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -30,8 +30,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $errors['telephone'] = !empty($_COOKIE['telephone_error']);
   $errors['email'] = !empty($_COOKIE['email_error']);
   $errors['year'] = !empty($_COOKIE['year_error']);
-  $errors['abilities[]'] = !empty($_COOKIE['abilities_error']);
+  $errors['abilities'] = !empty($_COOKIE['abilities_error']);
   $errors['radio-1'] = !empty($_COOKIE['radio_error']);
+  $errors['field-name-2'] = !empty($_COOKIE['field_error']);
   $errors['check-1'] = !empty($_COOKIE['check_error']);
   // TODO: аналогично все поля.
 
@@ -65,6 +66,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $messages[] = '<div class="error">Выберите языки.</div>';
   }
 
+  if ($errors['field-name-2']) {
+    setcookie('field_error', '', 100000);
+    $messages[] = '<div class="error">Введите биографию.</div>';
+  }
+
    if ($errors['check-1']) {
     setcookie('check_error', '', 100000);
     $messages[] = '<div class="error">Необходимо согласие.</div>';
@@ -78,6 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $values['year'] = empty($_COOKIE['year_value']) ? '' : $_COOKIE['year_value'];
   $values['radio-1'] = empty($_COOKIE['radio_value']) ? '' : $_COOKIE['radio_value'];
   $values['abilities'] = empty($_COOKIE['abilities_value']) ? '' : $_COOKIE['abilities_value'];
+  $values['field-name-2'] = empty($_COOKIE['field_value']) ? '' : $_COOKIE['field_value'];
   $values['check-1'] = empty($_COOKIE['check_value']) ? '' : $_COOKIE['check_value'];
   // TODO: аналогично все поля.
 
@@ -142,6 +149,14 @@ else {
     setcookie('abilities_value', $_POST['abilities'], time() + 30 * 24 * 60 * 60);
   }
 
+   if (empty($_POST['field-name-2'])) {
+    setcookie('field_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('field_value', $_POST['field'], time() + 30 * 24 * 60 * 60);
+  }
+
   if (empty($_POST['check-1'])) {
     setcookie('check_error', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
@@ -167,13 +182,42 @@ else {
     setcookie('email_error', '', 100000);
     setcookie('year_error', '', 100000); 
     setcookie('radio_error', '', 100000); 
-    setcookie('abilities_error', '', 100000); 
+    setcookie('abilities_error', '', 100000);
+    setcookie('field_error', '', 100000);
     setcookie('check_error', '', 100000); 
     // TODO: тут необходимо удалить остальные Cookies.
   }
 
   // Сохранение в БД.
-  
+  include('../db.php');
+$db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
+
+// Подготовленный запрос. Не именованные метки.
+try {
+  $stmt = $db->prepare("INSERT INTO application SET name = ?, phone = ?, email = ?, data = ?, pol = ?, bio = ?, ok = ?");
+  $stmt->execute([$_POST['fio'], $_POST['telephone'], $_POST['email'], $_POST['year'], $_POST['radio-1'], $_POST['field-name-2'], $_POST['check-1']]);
+  $application_id = $db->lastInsertId();
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+foreach ($_POST['abilities'] as $language) {
+
+  $stmt = $db->prepare("SELECT id FROM programming_languages WHERE language_name = ?");
+    $stmt->execute([$language]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) {
+        $language_id = $result['id'];
+      $stmt = $db->prepare("INSERT INTO application_languages (application_id, language_id) VALUES (?, ?)");
+        $stmt->execute([$application_id, $language_id]);
+    } else {
+        echo "Язык программирования '$language' не найден в базе данных.";
+    }
+}
 
   // Сохраняем куку с признаком успешного сохранения.
   setcookie('save', '1');
