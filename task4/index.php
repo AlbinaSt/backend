ecf2cba76f976b44a632356fd1d94a476e7e0737
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-04-22 20:47:37 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -151,7 +151,14 @@ else {
   
 
   if ($errors) {
-   
+
+    setcookie('fio_error', '1', time() + 24 * 60 * 60);
+    setcookie('telephone_error', '1', time() + 24 * 60 * 60);
+    setcookie('email_error', '1', time() + 24 * 60 * 60);
+    setcookie('radio_error', '1', time() + 24 * 60 * 60);
+    setcookie('check_error', '1', time() + 24 * 60 * 60);
+    setcookie('field_error', '1', time() + 24 * 60 * 60);
+    
     header('Location: index.php');
     exit();
   }
