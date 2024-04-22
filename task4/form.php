cc8b9a5671c375cb89de5169b3afdb2886cbe9f4
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-04-22 16:54:29 +0300

    Update form.php

--- task4/form.php
+++ task4/form.php
@@ -63,7 +63,7 @@ if (!empty($messages)) {
 // и задавая начальные значения элементов ранее сохраненными.
 ?>
 
-    <form action="" method="POST">
+    <form class="form" action="" method="POST">
       <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
       <input name="telephone" type="tel" <?php if ($errors['telephone']) {print 'class="error"';} ?> value="<?php print $values['telephone']; ?>" />
       <input name="email" type="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" />
