6aa00df49478e14efce6758a498787690f6c146a
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-04-22 17:15:08 +0300

    Update form.php

--- task4/form.php
+++ task4/form.php
@@ -67,11 +67,16 @@ if (!empty($messages)) {
 
     <form class="form" action="" method="POST">
       Введите имя: <input type="text" name="name" value="<?php echo isset($_COOKIE['name']) ? $_COOKIE['name'] : ''; ?>" pattern="[a-zA-Zа-яА-Я\s]+" title="Имя должно содержать только буквы и пробелы" required>
+      <br />
       Введите телефон: <input name="telephone" type="tel" <?php if ($errors['telephone']) {print 'class="error"';} ?> value="<?php print $values['telephone']; ?>" />
+      <br />
       Введите почту: <input name="email" type="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" />
+      <br />
       Введите дату рождения: <input name="year" type="date" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>" />
+      <br/>
       <input name="email" type="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" />
-              <label>
+      <br />
+      <label>
         Ваш пол:</label>
         <br />
           <class="radio-1"> <input
