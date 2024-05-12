f41334bfb314334b64b7d9863a82acf032d007ff
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-05-12 20:58:19 +0300

    Update index.php

--- task5/index.php
+++ task5/index.php
@@ -95,42 +95,61 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $values['field-name-2'] = empty($_COOKIE['field_value']) ? '' : $_COOKIE['field_value'];
   $values['check-1'] = empty($_COOKIE['check_value']) ? '' : $_COOKIE['check_value'];
 
-
+  if (session_start() && (!empty($_SESSION['login'])) && (!empty($_COOKIE[session_name()])) && $error) {
   // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
   // ранее в сессию записан факт успешного логина.
   include('../db.php');
   $db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
 
-  if (empty($errors) && !empty($_COOKIE[session_name()]) &&
-      session_start() && !empty($_SESSION['login'])) {
-        if ($stmt = $mysqli->prepare("SELECT name, telephone, email, data, pol, bio, ok FROM application WHERE uid = ?")) {
-        $stmt->bind_param("i", $_SESSION['uid']);
-        $stmt->execute();
-        $stmt->bind_result($_POST['fio'], $_POST['telephone'], $_POST['email'], $_POST['year'], $_POST['radio-1'], $_POST['field-name-2'], $_POST['check-1']);
-        
-        if ($stmt->fetch()) {
-            // Экранируем полученные данные
-            $_POST['fio'] = htmlspecialchars($fio, ENT_QUOTES);
-            $_POST['year'] = htmlspecialchars($_POST['date'], ENT_QUOTES);
-            $_POST['telephone'] = htmlspecialchars($_POST['telephone'], ENT_QUOTES);
-            $_POST['email'] = htmlspecialchars($email, ENT_QUOTES);
-            $_POST['radio-1'] = htmlspecialchars($POST_['pol'], ENT_QUOTES);
-            $_POST['field-name-2'] = htmlspecialchars($_POST['bio'], ENT_QUOTES);
-            $_POST['check-1'] = htmlspecialchars($_POST['ok'], ENT_QUOTES);
-
-           $values = array(
-                'fio' => $fio,
-                'date' => $date,
-                'telephone' => $telephone,
-                'email' => $email
-            );
-            
-
-    printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
-  }
-		$stmt->close();
+  try{
+    $stmt = $db->prepare("SELECT user_id  FROM users WHERE login = :login");
+    $stmt->execute(['login' => $_SESSION['login']]);
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    $stmt = $db->prepare("SELECT l.name
+    FROM application_languages AS a JOIN programming_language AS l ON a.language_id = l.id WHERE a.application_id = :user_id");
+    $stmt->execute(['user_id' => $data['user_id']]);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $languages = [];
+    foreach ($rows as $row) {
+        $languages[] = htmlspecialchars($row['name']);
     }
+
+    $abilities_serialized = serialize($languages);
+    
+    $stmt = $db->prepare("SELECT name, phone, email, data, pol, bio, ok  FROM application WHERE user_id = :user_id");
+    $stmt->execute(['user_id' => $data['user_id']]);
+    $row = $stmt->fetch();
+
+           $values = [
+        'name' => htmlspecialchars($row['fio']),
+        'phone' => htmlspecialchars($row['telephone']),
+        'email' => htmlspecialchars($row['email']),
+        'data' => htmlspecialchars($row['year']),
+        'pol' => htmlspecialchars($row['radio-1']),
+        'bio' => htmlspecialchars($row['field-name-2']),
+        'ok' => htmlspecialchars($row['check-1']),
+        'abilities' => $languages
+    ];
+  }
+            
+    catch(PDOException $e){
+      print('Error : ' . $e->getMessage());
+      exit();
+    }
+    setcookie('name_value',$row['name'], time() + 30 * 24 * 60 * 60);
+    setcookie('phone_value',$row['phone'], time() + 30 * 24 * 60 * 60);
+    setcookie('email_value',$row['email'], time() + 30 * 24 * 60 * 60);
+    setcookie('data_value',$row['data'], time() + 30 * 24 * 60 * 60);
+    setcookie('pol_value',$row['pol'], time() + 30 * 24 * 60 * 60);
+    setcookie('bio_value',$row['bio'], time() + 30 * 24 * 60 * 60);
+    setcookie('ok_value',$row['ok'], time() + 30 * 24 * 60 * 60);
+    setcookie('abilities_value', $abilities_serialized, time() + 30 * 24 * 60 * 60);
+
+	  
+    printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
+  
 }
 
   // Включаем содержимое файла form.php.
@@ -199,7 +218,7 @@ else {
     $errors = TRUE;
   }
   else {
-    setcookie('field_value', $_POST['field'], time() + 30 * 24 * 60 * 60);
+    setcookie('field_value', $_POST['field-name-2'], time() + 30 * 24 * 60 * 60);
   }
 
   if (empty($_POST['check-1'])) {
@@ -207,13 +226,28 @@ else {
     $errors = TRUE;
   }
   else {
-    setcookie('check_value', $_POST['check'], time() + 30 * 24 * 60 * 60);
+    setcookie('check_value', $_POST['check-1'], time() + 30 * 24 * 60 * 60);
   }
 
   if ($errors) {
     header('Location: index.php');
     exit();
   }
+  setcookie('fio_value', $_POST['name'], time() + 365 * 24 * 60 * 60);
+  setcookie('telephone_value', $_POST['phone'], time() + 365 * 24 * 60 * 60);
+  setcookie('email_value', $_POST['email'], time() + 365 * 24 * 60 * 60);
+  setcookie('year_value', $_POST['data'], time() + 365 * 24 * 60 * 60);
+  setcookie('radio_value', $_POST['pol'], time() + 365 * 24 * 60 * 60);
+  setcookie('abilities_value', serialize($_POST['abilities']), time() + 365 * 24 * 60 * 60);
+  setcookie('field_value', $_POST['bio'], time() + 365 * 24 * 60 * 60);
+  setcookie('check_value', $_POST['ok'], time() + 365 * 24 * 60 * 60);
+
+  if ($errors) {
+    // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
+    header('Location: index.php');
+    exit();
+  }
+	  
   else {
     setcookie('fio_error', '', 100000);
     setcookie('telephone_error', '', 100000);
@@ -239,7 +273,7 @@ else {
 	  $pass = ''; 
 	  while($length--) {
 	   	$pass .= $chars[random_int(0, $size)]; 
-      $login .= $chars[random_int(0, $size)]; 
+    $login .= $chars[random_int(0, $size)]; 
     }
     // Сохраняем в Cookies.
     setcookie('login', $login);
