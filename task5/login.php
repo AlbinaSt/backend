d827a897a584efe4345ee5bc235096881c189bb5
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-05-12 17:12:00 +0300

    Update login.php

--- task5/login.php
+++ task5/login.php
@@ -1,58 +1,94 @@
 <?php
 
-/**
- * Файл login.php для не авторизованного пользователя выводит форму логина.
- * При отправке формы проверяет логин/пароль и создает сессию,
- * записывает в нее логин и id пользователя.
- * После авторизации пользователь перенаправляется на главную страницу
- * для изменения ранее введенных данных.
- **/
-
-// Отправляем браузеру правильную кодировку,
-// файл login.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
-// В суперглобальном массиве $_SESSION хранятся переменные сессии.
-// Будем сохранять туда логин после успешной авторизации.
 $session_started = false;
 if ($_COOKIE[session_name()] && session_start()) {
   $session_started = true;
   if (!empty($_SESSION['login'])) {
-    // Если есть логин в сессии, то пользователь уже авторизован.
-    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
-    //при нажатии на кнопку Выход).
-    // Делаем перенаправление на форму.
+      if (!empty($_COOKIE['logout'])) {
+            setcookie('logout', '', 100000);
+            session_destroy();
+        }
     header('Location: ./');
     exit();
   }
 }
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    setcookie('error', '', 100000);
+    $messages[] = '<div class="error">Неверный логин или пароль</div>';
+  }
+ 
+$values = array();
+$values['login'] = empty($_COOKIE['login_value']) ? '' : strip_tags($_COOKIE['login_value']);
+$values['password'] = empty($_COOKIE['password_value']) ? '' : strip_tags($_COOKIE['password_value']);
+
 ?>
 
+<body>
 <form action="" method="post">
-  <input name="login" />
-  <input name="pass" />
+  <?php 
+  if (!empty($messages)) {
+  print('<div id="messages">');
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+?>
+  <input name="login" <?php if ($errors['error']) {print 'class="error"';} ?> value="<?php print $values['login']; ?>"/>
+  <input name="password" <?php if ($errors['error']) {print 'class="error"';} ?> value="<?php print $values['password']; ?>"/>
   <input type="submit" value="Войти" />
 </form>
-
+  </body>
+</html>
+  
 <?php
 }
-// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
+
 else {
-  // TODO: Проверть есть ли такой логин и пароль в базе данных.
-  // Выдать сообщение об ошибках.
+    include('../db.php');
+    $db = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_login, $db_pass,
+   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+    $login = $_POST['login'];
+    $password = $_POST['password'];
 
-  if (!$session_started) {
-    session_start();
-  }
-  // Если все ок, то авторизуем пользователя.
-  $_SESSION['login'] = $_POST['login'];
-  // Записываем ID пользователя.
-  $_SESSION['uid'] = 123;
+    try{
+    $stmt = $db->prepare("SELECT * FROM users WHERE login = :login");
+    $stmt->execute(array(':login' => $login));
+    $use = $stmt->fetch();
 
-  // Делаем перенаправление.
+    setcookie('login_value', $_POST['login'], time() + 30 * 24 * 60 * 60);
+    setcookie('password_value', $_POST['password'], time() + 30 * 24 * 60 * 60);
+  
+    if ($use && password_verify($password, $use['password'])) {
+      
+    setcookie('login_value', '', 100000);
+    setcookie('password_value', '', 100000);
+    } else {
+     setcookie('error', '1', time() + 24 * 60 * 60);
+     header('Location: login.php');
+     exit();
+    }
+    if (!$session_started) {
+      session_start();
+    }
+      
+   $login = $_POST['login'];
+   $password = $_POST['password'];
+   $_SESSION['login'] = $_POST['login'];
+      
+   $stmt = $db->prepare("SELECT user_id  FROM users WHERE login = :login");
+    $stmt->execute([':login' => $login]);
+   $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+   $_SESSION['uid'] = $data['user_id'];
+
+   } catch(PDOException $e){
+     print('Error : ' . $e->getMessage());
+     exit();
+   }
+
   header('Location: ./');
 }
