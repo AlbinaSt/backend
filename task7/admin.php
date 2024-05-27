5e017c635b71d2094d6c30bfd695543e39c12bb0
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2024-05-27 22:17:17 +0300

    Update admin.php

--- task7/admin.php
+++ task7/admin.php
@@ -1,26 +1,88 @@
 <?php
 
-/**
- * Задача 6. Реализовать вход администратора с использованием
- * HTTP-авторизации для просмотра и удаления результатов.
- **/
-
-// Пример HTTP-аутентификации.
-// PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
-// Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
-if (empty($_SERVER['PHP_AUTH_USER']) ||
-    empty($_SERVER['PHP_AUTH_PW']) ||
-    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
-  header('HTTP/1.1 401 Unanthorized');
-  header('WWW-Authenticate: Basic realm="My site"');
-  print('<h1>401 Требуется авторизация</h1>');
-  exit();
-}
+require_once('database.php'); 
+require_once('authorization.php');
 
 print('Вы успешно авторизовались и видите защищенные паролем данные.');
 
-// *********
-// Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
-// Реализовать просмотр и удаление всех данных.
-// *********
+?>
+
+    <form action="" method="POST">
+            <input name="delete"/>
+          <input type="submit" name = "button" value="Delete" />
+            <input name="update"/>
+          <input type="submit" name = "button" value="Update" />
+    </form>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    if($_POST['button'] == "Delete")
+    {
+        if(!empty($_POST['delete']))
+    {
+    $user_id = $_POST['delete'];
+    $result = get_user_id($user_id);
+
+if ($result) {
+    $result = delete_by_id($user_id);
+    echo "Данные успешно удалены. <br>";
+    header('Location: admin.php');
+    exit();
+}
+else {
+    echo "Id не найден в базе данных. <br>";
+}
+    }
+    else{
+            echo "Заполните id <br>";
+        }
+    }
+        if($_POST['button'] == "Update")
+    {
+        if(!empty($_POST['update']))
+        {
+        session_start();
+      $user_id = $_POST['update'];
+    $result = get_user_id($user_id);
+    if ($result) {
+    $data = get_login($user_id);
+    $_SESSION['login'] = $data['login'];
+
+    $_SESSION['uid'] = $user_id;
+    header('Location: index.php');
+        exit();
+    }
+    else {
+    echo "Id не найден в базе данных. <br>";
+}
+    }
+        else{
+            echo "Заполните id <br>";
+        }
+    }
+}
+$results = get_all_user();
+
+    foreach ($results as $row) {
+        echo "Пользователь с login " . $row['login'] ." и id ". $row['id'] . "<br>";
+        echo "Full name: " . $row['name'] . "<br>";
+        echo "Telephone: " . $row['phone'] . "<br>";
+        echo "Email: " . $row['email'] . "<br>";
+        echo "Date: " . $row['data'] . "<br>";
+        echo "Gender: " . $row['pol'] . "<br>";
+        echo "Biography: " . $row['bio'] . "<br>";
+        echo "Agreement: " . $row['ok'] . "<br>";
+        echo "Languages: " . $row['languages'] . "<br><br>";
+    }
+    echo "Статистика языков: " . "<br />";
+    $query = "SELECT l.language_name, count(*) AS count_users
+            FROM application a 
+            INNER JOIN application_languages al ON a.id = al.application_id
+            INNER JOIN programming_languages l ON al.language_id = l.id
+            GROUP BY l.language_name";
+
+ $languages = get_status_language();
+    foreach ($languages as $row) {
+        echo "Язык {$row['language_name']} любят {$row['count_users']} пользователя <br>";
+    }
+?>
